e963508399c416d3e48cf32343fd6e7b16b01f7b
ukmkokap2 <ukmkokap2@gmail.com>
2026-02-20 23:21:47 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,29 +9,94 @@ import { supabase } from '../lib/supabaseClient'
 export default function Home() {
   const [events, setEvents] = useState<any[]>([])
 
+  // daftar petugas
+  const petugasList = [
+    "dr. Arum Ermi Wijayanti",
+    "dr. Erry Kurniawan",
+    "drg. Purba Pramana",
+    "Iwan Riswandi, AMK",
+    "Insana Prasetya, AMK",
+    "Susilowati, A.Md.Kep",
+    "Suminten, A.Md",
+    "Cahyani Agustina Prihastuti, S.Tr.Keb",
+    "Tri Murni, A.Md.Keb",
+    "Siti Khotijah, S.Tr.Keb",
+    "Lilian Adven CN, S.Tr.Keb",
+    "Noor Syamsiah, S.Tr.Keb",
+    "Hadrotul Ma'wa, S.Tr.Kes",
+    "Yuniarti Parptiwi, AMAK",
+    "Latiful Hakim, AMD",
+    "Widiarto, SST",
+    "Sahir, AMKL",
+    "Setyo Hadiriyanto, A.Md",
+    "Ade Doni Irawan, A.Md.Farm",
+    "Tri Anggoro, S.M.",
+    "Ria Wahyu Dewanti, S.Farm.Apt",
+    "Siti Hidayati, S.Far.Apt",
+    "Zelin Afika, SE",
+    "Devta Virga Nirwana",
+    "Muh Tozidaini",
+    "Khasanatun Masruroh, S.Gz.",
+    "Rina Prasetya, Amd. Keb.",
+    "Kardiyono",
+    "Rina Dwi Mawarti",
+    "Dwi Tyas Wardani",
+    "Sutriyanto",
+    "Sunarta",
+    "NINIK SRIWIJIASTUTI",
+    "MUJIYANA",
+    "MUJIMAN",
+    "HARYANTA",
+    "ANTO ARIYANTO",
+    "ENIK MULYANI",
+    "ERNI KUSRINI",
+    "AMIRUDIN",
+    "FEBRI EKO SUSANTI",
+    "SUWANTI",
+    "FUAD WARDANI",
+    "MARJOKO"
+  ]
+
   useEffect(() => {
     fetchData()
   }, [])
 
   async function fetchData() {
     const { data } = await supabase.from('kegiatan').select('*')
+
     if (data) {
       setEvents(
-        data.map(item => ({
-          title: item.nama_kegiatan,
-          date: item.tanggal
+        data.map((item) => ({
+          title: `${item.nama_kegiatan}${item.penanggung_jawab ? " - " + item.penanggung_jawab : ""}`,
+          date: item.tanggal,
         }))
       )
     }
   }
 
-  async function handleDateClick(info:any) {
-    const nama = prompt("Nama kegiatan:")
-    if (!nama) return
+  async function handleDateClick(info: any) {
+
+    const pilihan = prompt(
+      "Pilih nomor petugas:\n\n" +
+      petugasList.map((p, i) => `${i + 1}. ${p}`).join("\n")
+    )
+
+    if (!pilihan) return
+
+    const namaPetugas = petugasList[Number(pilihan) - 1]
+
+    if (!namaPetugas) {
+      alert("Nomor tidak valid")
+      return
+    }
+
+    const kegiatan = prompt("Nama kegiatan:")
+    if (!kegiatan) return
 
     await supabase.from('kegiatan').insert({
       tanggal: info.dateStr,
-      nama_kegiatan: nama
+      nama_kegiatan: kegiatan,
+      penanggung_jawab: namaPetugas
     })
 
     fetchData()
